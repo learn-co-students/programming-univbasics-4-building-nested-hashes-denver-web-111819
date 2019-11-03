0e9f526cb3a93e8daa242007f1b2d48a5c3af268
Karl Matthes <karlmatthes@gmail.com>
2019-11-03 21:46:37 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,10 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-  {:railroads => {:pieces => 4}}
+  import_hash = base_hash
+  import_hash[:railroads][:pieces] = 4
+  
+  return import_hash
   
 end
 
@@ -22,22 +25,23 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-  {
-    :railroads => {
-      :rent_in_dollars => {
-        :one_piece_owned => 25,
-        :two_pieces_owned => 50,
-        :three_pieces_owned => 100,
-        :four_pieces_owned => 200
-      },
-      :names => {
-        :reading_railroad => {},
-        :pennsylvania_railroad => {},
-        :b_and_o_railroad => {},
-        :shortline_railroad => {}
-      }
-    }
+  import_hash = monopoly_with_second_tier
+  
+  import_hash[:railroads][:rent_in_dollars] = {
+    :one_piece_owned => 25,
+    :two_pieces_owned => 50,
+    :three_pieces_owned => 100,
+    :four_pieces_owned => 200
   }
+  
+  import_hash[:railroads][:names] ={
+    :reading_railroad => {},
+    :pennsylvania_railroad => {},
+    :b_and_o_railroad => {},
+    :shortline_railroad => {}
+  }
+  
+  return import_hash
   
 end
 
@@ -47,16 +51,13 @@ def monopoly_with_fourth_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
+  import_hash = monopoly_with_third_tier
   
-  {
-    :railroads => {
-      :names => {
-        :reading_railroad => {"mortgage_value" => "$100"},
-        :pennsylvania_railroad => {"mortgage_value" => "$200"},
-        :b_and_o_railroad => {"mortgage_value" => "$400"},
-        :shortline_railroad => {"mortgage_value" => "$800"}
-      }
-    }
-  }
+  import_hash[:railroads][:names][:reading_railroad] = {"mortgage_value" => "$100"}
+  import_hash[:railroads][:names][:pennsylvania_railroad] = {"mortgage_value" => "$200"}
+  import_hash[:railroads][:names][:b_and_o_railroad] = {"mortgage_value" => "$400"}
+  import_hash[:railroads][:names][:shortline_railroad] = {"mortgage_value" => "$800"}
+  
+  return import_hash
   
 end
